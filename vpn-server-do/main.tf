d9eeeffd3b35403e19dc66de11a0aaed61b1583a
Michal Schott <m.schott@kainos.com>
2017-03-08 10:50:48 +0100

    Generate networkconnect mac file with VPN settings.

--- vpn-server-do/main.tf
+++ vpn-server-do/main.tf
@@ -14,9 +14,21 @@ variable "ssh_keys" {
 
 variable "vpn_ipsec_psk" {}
 
+variable "vpn_ipsec_psk_encrypted" {
+  default = ""
+}
+
 variable "vpn_username" {}
 
 variable "vpn_password" {}
+
+variable "vpn_password_encrypted" {
+  default = ""
+}
+
+variable "networkconnect_file_location" {
+  default = "~/Desktop/VPN.networkconnect"
+}
 
 provider "digitalocean" {
   token = "${var.do_token}"
@@ -32,6 +44,17 @@ data "template_file" "userdata" {
   }
 }
 
+data "template_file" "networkconnect" {
+  template = "${file("${path.module}/vpn.networkconnect.tpl")}"
+
+  vars {
+    vpn_ipsec_psk_encrypted = "${var.vpn_ipsec_psk_encrypted}"
+    vpn_username            = "${var.vpn_username}"
+    vpn_password_encrypted  = "${var.vpn_password_encrypted}"
+    ip_address              = "${digitalocean_droplet.vpn.ipv4_address}"
+  }
+}
+
 resource "digitalocean_droplet" "vpn" {
   image     = "ubuntu-16-04-x64"
   name      = "vpn"
@@ -39,6 +62,12 @@ resource "digitalocean_droplet" "vpn" {
   size      = "${var.size}"
   ssh_keys  = "${var.ssh_keys}"
   user_data = "${data.template_file.userdata.rendered}"
+}
+
+resource "null_resource" "mac_config_file" {
+  provisioner "local-exec" {
+    command = "echo \"${data.template_file.networkconnect.rendered}\" > ${var.networkconnect_file_location}"
+  }
 }
 
 output "IP" {
