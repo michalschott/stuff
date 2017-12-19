451b4da2b8e0ebd08cdea5c0b9cf9a60489786e7
Michal Schott <schott.michal@gmail.com>
2017-12-19 10:20:55 +0100

    Update for TF 0.11.X

--- vpn-server-do/start.sh
+++ vpn-server-do/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-terraform apply
+terraform apply -auto-approve
 IP=`terraform output | grep IP | awk '{print $3}'`
 sleep 15
 ssh root@${IP} tail -f /var/log/cloud-init-output.log
-
